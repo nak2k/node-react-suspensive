1966c7b987aa0603b3be7c47c0413b52d0d828b7
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2020-10-24 19:00:50 +0900

    Refactor

---
+++ src/Suspensive.ts
@@ -0,0 +1,37 @@
+/**
+ * Promise wrapper for render-as-you-fetch.
+ *
+ * ```typescript
+ * function ResourceWithLoading() {
+ *   const resource = new Suspensive(fetch(...));
+ *
+ *   return (
+ *     <Suspense fallback={<Loading />}>
+ *       <Resource resource={resource} />
+ *     </Suspense>
+ *   );
+ * }
+ *
+ * function Resource(props: { resource: Suspensive<Resource>}) {
+ *   // If the value not prepared, throw an exception at the following line.
+ *   const resource = props.resource.value;
+ *   ...
+ * }
+ * ```
+ */
+export class Suspensive<T> {
+  private _get: () => T;
+
+  constructor(promise: Promise<T>) {
+    this._get = () => { throw promise };
+
+    promise.then(
+      value => this._get = () => value,
+      reason => this._get = () => { throw reason }
+    );
+  }
+
+  get value() {
+    return this._get();
+  }
+}
